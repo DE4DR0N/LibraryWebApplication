a2db63d5c554e3437c98a93a432a08006a5aeaf9
Eugene <eugene.gedrevich@outlook.com>
2024-10-01 08:00:50 +0300

    Hotfix

--- backend/LibraryWebApp/LibraryWebApp.Tests/Repositories/BooksRepositoryTests.cs
+++ backend/LibraryWebApp/LibraryWebApp.Tests/Repositories/BooksRepositoryTests.cs
@@ -1,57 +1,59 @@
 ﻿using LibraryWebApp.Domain.Entities;
 using LibraryWebApp.Persistence;
 using LibraryWebApp.Persistence.Repositories;
-using LibraryWebApp.Tests;
 using Microsoft.EntityFrameworkCore;
 
-public class BooksRepositoryTests
+namespace LibraryWebApp.Tests.Repositories
 {
-    private BooksRepository _repository;
-    private LibraryWebAppDbContext _context;
-
-    public BooksRepositoryTests()
+    public class BooksRepositoryTests
     {
-        _context = TestDbContext.Create();
-        _repository = new BooksRepository(_context);
-    }
+        private BooksRepository _repository;
+        private LibraryWebAppDbContext _context;
 
-    [Fact]
-    public async Task GetByIsbnAsync_ShouldReturnBook_WhenBookExists()
-    {
-        // Arrange
-        long isbn = 123456789;
-
-        // Act
-        var result = await _repository.GetByIsbnAsync(isbn);
-
-        // Assert
-        Assert.NotNull(result);
-        Assert.Equal(isbn, result.ISBN);
-    }
-
-    [Fact]
-    public async Task AddAsync_ShouldAddBookToDatabase()
-    {
-        var author = _context.Authors.First();
-        // Arrange
-        var newBook = new BookEntity
+        public BooksRepositoryTests()
         {
-            Id = Guid.NewGuid(),
-            ISBN = 987654321,
-            Title = "Title2",
-            Genre = "Fantasy",
-            Description = "Who moves fisrt is :)",
-            AuthorId = _context.Authors.First().Id,
-            Author = author
-        };
+            _context = TestDbContext.Create();
+            _repository = new BooksRepository(_context);
+        }
 
-        // Act
-        await _repository.AddAsync(newBook);
-        await _context.SaveChangesAsync();
+        [Fact]
+        public async Task GetByIsbnAsync_ShouldReturnBook_WhenBookExists()
+        {
+            // Arrange
+            long isbn = 123456789;
 
-        // Assert
-        var bookFromDb = await _context.Books.FirstOrDefaultAsync(b => b.Id == newBook.Id);
-        Assert.NotNull(bookFromDb);
-        Assert.Equal(newBook.Title, bookFromDb.Title);
+            // Act
+            var result = await _repository.GetByIsbnAsync(isbn);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(isbn, result.ISBN);
+        }
+
+        [Fact]
+        public async Task AddAsync_ShouldAddBookToDatabase()
+        {
+            var author = _context.Authors.First();
+            // Arrange
+            var newBook = new BookEntity
+            {
+                Id = Guid.NewGuid(),
+                ISBN = 987654321,
+                Title = "Title2",
+                Genre = "Fantasy",
+                Description = "Who moves fisrt is :)",
+                AuthorId = _context.Authors.First().Id,
+                Author = author
+            };
+
+            // Act
+            await _repository.AddAsync(newBook);
+            await _context.SaveChangesAsync();
+
+            // Assert
+            var bookFromDb = await _context.Books.FirstOrDefaultAsync(b => b.Id == newBook.Id);
+            Assert.NotNull(bookFromDb);
+            Assert.Equal(newBook.Title, bookFromDb.Title);
+        }
     }
 }
