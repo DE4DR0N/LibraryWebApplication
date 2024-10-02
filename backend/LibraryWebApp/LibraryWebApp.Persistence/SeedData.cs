3f5739829ea54dc5c00ef4fba2c3d37dabaaf22c
Eugene <eugene.gedrevich@outlook.com>
2024-10-02 19:44:36 +0300

    Create Authors

--- backend/LibraryWebApp/LibraryWebApp.Persistence/SeedData.cs
+++ backend/LibraryWebApp/LibraryWebApp.Persistence/SeedData.cs
@@ -14,8 +14,52 @@ namespace LibraryWebApp.Persistence
                 var unitOfWork = services.GetRequiredService<IUnitOfWork>();
 
                 await SeedAdminUserAsync(unitOfWork);
+                await SeedAuthorsAsync(unitOfWork);
             }
         }
+
+        private static async Task SeedAuthorsAsync(IUnitOfWork unitOfWork)
+        {
+            var authors = new List<AuthorEntity>
+            {
+                new AuthorEntity
+                {
+                    Id = Guid.NewGuid(),
+                    FirstName = "Jane",
+                    LastName = "Austen",
+                    BirthDate = new DateOnly(1775, 12, 16),
+                    Country = "United Kingdom"
+                },
+                new AuthorEntity
+                {
+                    Id = Guid.NewGuid(),
+                    FirstName = "Mark",
+                    LastName = "Twain",
+                    BirthDate = new DateOnly(1835, 11, 30),
+                    Country = "United States"
+                },
+                new AuthorEntity
+                {
+                    Id = Guid.NewGuid(),
+                    FirstName = "Fyodor",
+                    LastName = "Dostoevsky",
+                    BirthDate = new DateOnly(1821, 11, 11),
+                    Country = "Russia"
+                }
+            };
+
+            foreach (var author in authors)
+            {
+                var existingAuthor = await unitOfWork.Authors.GetByIdAsync(author.Id);
+                if (existingAuthor == null)
+                {
+                    await unitOfWork.Authors.AddAsync(author);
+                }
+            }
+
+            await unitOfWork.CompleteAsync();
+        }
+
 
         private static async Task SeedAdminUserAsync(IUnitOfWork unitOfWork)
         {
