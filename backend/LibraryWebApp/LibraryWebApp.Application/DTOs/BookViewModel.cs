8e4d2e78e074f838c42f89cd5bff67a3741d40cc
DE4DR0N <eugene.gedrevich@outlook.com>
2024-10-03 12:10:32 +0300

    Hide AuthorViewModel

--- backend/LibraryWebApp/LibraryWebApp.Application/DTOs/BookViewModel.cs
+++ backend/LibraryWebApp/LibraryWebApp.Application/DTOs/BookViewModel.cs
@@ -3,12 +3,12 @@
     public class BookViewModel
     {
         public Guid Id { get; set; }
-        public long ISBN { get; set; }
+        public required long ISBN { get; set; }
         public required string Title { get; set; }
         public required string Genre { get; set; }
         public required string Description { get; set; }
-        public Guid AuthorId { get; set; }
-        public AuthorViewModel Author { get; set; }
+        public required Guid AuthorId { get; set; }
+        //public AuthorViewModel Author { get; set; }
         public Guid? UserId { get; set; }
         public DateOnly? BorrowDate { get; set; }
         public DateOnly? ReturnDate { get; set; }
